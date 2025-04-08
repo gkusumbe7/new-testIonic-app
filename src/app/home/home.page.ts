d6cf41f142b697a9ee4a99f68e939282957a3906
Ganesh Kusumbe <ganeshkusumbe@Ganeshs-MacBook-Air.local>
2025-04-08 23:57:54 +0530

    added notification page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonAvatar, IonSearchbar, IonCard, IonImg } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonImg, IonCard, IonSearchbar, IonAvatar,  IonContent],
 })
 export class HomePage {
   constructor() {}
